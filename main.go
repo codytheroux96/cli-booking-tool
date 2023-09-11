1081808a6ce395f2a03395b7806deed3e255a08b
Cody Theroux <cody.theroux3@gmail.com>
2023-09-10 21:54:21 -0400

    made variables for the conference name, starting amount of tickets, and value for remaining tickets

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
-}
+	const conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
+	fmt.Println("Welcome to the", conferenceName, "booking application!")
+	fmt.Println("Get your tickets here to attend!")
+	fmt.Println("We have a total of", conferenceTickets, "and", remainingTickets, "are still available.")
+
+
+
+}
