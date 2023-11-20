7cabd7f60297f5382a8f1a9d9b21fde7d001f2e8
Cody Theroux <cody.theroux3@gmail.com>
2023-11-19 19:34:43 -0500

    collecting user input

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	const conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	remainingTickets  := 50
 
 	fmt.Printf("Welcome to the %v booking application!\n", conferenceName)
 	fmt.Printf("Get your tickets here to attend!\n")
-	fmt.Printf("We have a total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println(("Enter your first name: "))
+	fmt.Scan(&firstName)
+
+	fmt.Println(("Enter your last name: "))
+	fmt.Scan(&lastName)
+
+	fmt.Println(("Enter your email: "))
+	fmt.Scan(&email)
+
+	fmt.Println(("Enter number of tickets: "))
+	fmt.Scan(&userTickets)
+	
+	fmt.Printf("Thank you for %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
